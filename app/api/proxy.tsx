5f9a359f3b21122c2c5656ef4d46f518e01f8c0d
Hadisa <afg.norozi@gmail.com>
2023-12-24 15:24:54 +0430

    fix

--- app/api/proxy.tsx
+++
@@ -1,8 +0,0 @@
-// api/proxy.js
-const fetch = require('node-fetch')
-
-module.exports = async (req, res) => {
-  const response = await fetch('http://your-http-backend.com' + req.url)
-  const data = await response.json()
-  res.json(data)
-}
