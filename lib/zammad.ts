4f50cb1fc1ea4be49db52e0b05e62128958dcf0b
Hadisa <afg.norozi@gmail.com>
2023-12-24 16:06:25 +0430

    fix

--- lib/zammad.ts
+++ lib/zammad.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Import necessary modules and types
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
 import crypto from 'crypto'
